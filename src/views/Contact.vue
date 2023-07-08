deb35b4831e5cb3112de299f84075cb80b0a28dd
Adam <hviidadam@gmail.com>
2023-07-08 13:54:54 +0200

    emailjs implemented

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,19 +4,12 @@
       <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Kom i kontakt</h2>
       <p class="mt-2 text-lg leading-8 text-gray-600">en tekst hernede?</p>
     </div>
-    <form action="#" method="POST" class="mx-auto mt-5 max-w-xl sm:mt-20">
+    <form @submit.prevent="sendEmail" class="mx-auto mt-5 max-w-xl sm:mt-20">
       <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-        <div>
-          <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900">Fornavn</label>
+        <div class="sm:col-span-2">
+          <label for="name" class="block text-sm font-semibold leading-6 text-gray-900">Navn</label>
           <div class="mt-2.5">
-            <input type="text" name="first-name" id="first-name" autocomplete="given-name"
-              class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-          </div>
-        </div>
-        <div>
-          <label for="last-name" class="block text-sm font-semibold leading-6 text-gray-900">Efternavn</label>
-          <div class="mt-2.5">
-            <input type="text" name="last-name" id="last-name" autocomplete="family-name"
+            <input type="text" name="name" id="name"
               class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
@@ -25,13 +18,6 @@
           <div class="mt-2.5">
             <input type="email" name="email" id="email" autocomplete="email"
               class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-          </div>
-        </div>
-        <div class="sm:col-span-2">
-          <label for="phone-number" class="block text-sm font-semibold leading-6 text-gray-900">Telefon Nummer</label>
-          <div class="relative mt-2.5">
-            <input type="tel" name="phone-number" id="phone-number" autocomplete="tel"
-              class="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
         <div class="sm:col-span-2">
@@ -45,15 +31,55 @@
       <div class="mt-7">
         <button type="submit"
           class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-          Indsend
+          <span v-if="!loading">Send</span>
+          <div v-else class="flex justify-center">
+            <div class="w-5 h-5 border-2 border-indigo-200 rounded-full animate-spin"></div>
+          </div>
         </button>
       </div>
     </form>
   </div>
 </template>
 
-<script setup>
-import { ref } from 'vue'
+<script>
+//https://dashboard.emailjs.com/admin/account
+import emailjs from 'emailjs-com';
 
-const agreed = ref(false)
+export default {
+  name: 'ContactMe',
+  data() {
+    return {
+      email: '',
+      name: '',
+      message: '',
+      loading: false,
+    }
+  },
+  methods: {
+    async sendEmail() {
+      this.loading = true;
+      try {
+        await emailjs.sendForm(
+          import.meta.env.VITE_EMAILJS_SERVICE_ID,
+          import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+          e.target,
+          import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
+          {
+            email: this.email,
+            name: this.name,
+            message: this.meessage
+          }
+        );
+      } catch (error) {
+
+        console.error(error);
+      }
+
+      this.loading = false;
+      this.email = '';
+      this.name = '';
+      this.message = '';
+    },
+  }
+}
 </script>
